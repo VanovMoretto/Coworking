3bc058bf9f183e893fc7e0f19e985eaa209dbe6a
VanovMoretto <vanovmorettous@gmail.com>
2023-05-02 20:06:16 -0300

    added some css features on responsive layout

--- src/App.js
+++ src/App.js
@@ -5,27 +5,28 @@ import Button from './components/Button';
 import Navbar from './components/Navbar';
 
 function App() {
+  
   return (
     <div className="parent-container">
-      <Navbar/>
+      <Navbar />
       <div className="main-container">
-      <div className="card">
-      <Sala title="Térreo"/>
-      <Button/>
+        <div className="card">
+          <Sala title="Térreo" />
+          <Button />
+        </div>
+        <div className="card">
+          <Sala title="Podcast" />
+          <Button />
+        </div>
+        <div className="card">
+          <Sala title="Mezanino" />
+          <Button />
+        </div>
+        <div className="card">
+          <Sala title="Arena" />
+          <Button />
+        </div>
       </div>
-      <div className="card">
-      <Sala title="Podcast"/>
-      <Button/>
-      </div>
-      <div className="card">
-      <Sala title="Mezanino"/>
-      <Button/>
-      </div>
-      <div className="card">
-      <Sala title="Arena"/>
-      <Button/>
-      </div>
-    </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css'
 import logo from '../imgs/D.png'
 
+export default () => {
+    const [showMenu, setShowMenu] = useState(false); // Adiciona um estado para controlar a exibição do menu
 
-export default props => {
+    const toggleClick = () => {
+        
+    }
+
     return (
-        <nav className="navbar">
+        <div className="navbar">
             <div className="navbar-logo">
                 <img src={logo} alt="Logo" />
                 <h3 className="titulo"><strong>Método Dutra</strong></h3>
             </div>
-            <div className="navbar-buttons">
-            <button className="login">Login</button>
-            <button className="signup">Registrar-se</button>
-            </div>
-        </nav>
+            <nav className="menu-section">
+                <div className="navbar-buttons">
+                    <ul>
+                        <li>
+                            <a href="" className="login">Login</a>
+                        </li>
+                        <li>
+                            <a href="" className="signup">Registrar-se</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="menu-toggle" onClick={toggleClick}>
+                    <div className="one"></div>
+                    <div className="two"></div>
+                    <div className="three"></div>
+                </div>
+            </nav>
+        </div>
     )
 }
