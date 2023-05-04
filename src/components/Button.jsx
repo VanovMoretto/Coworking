95646bdfffdb0cd22632e2e2a1fdadb655c8713d
Vanov Moretto - Dutra Corporativo <vanov@dutracorporativo.com.br>
2023-05-04 17:27:58 -0300

    Component button's code improved

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,95 +2,70 @@ import React, { useState } from "react";
 import "./Button.css";
 
 export default function Button(props) {
-  const [setExibirX] = useState(true);
   const [showHorarios, setShowHorarios] = useState(false);
   const [horarioSelecionado, setHorarioSelecionado] = useState("");
   const [horarioInicial, setHorarioInicial] = useState("");
   const [horarioFinal, setHorarioFinal] = useState("");
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [horarioReservado, setHorarioReservado] = useState("");
-  const [reservas, setReservas] = useState([]);
-  const [primeiroHorarioSelecionado, setPrimeiroHorarioSelecionado] = useState(false);
 
   const handleHorariosClick = (event) => {
     setShowHorarios(true);
-    setPosition({ x: event.clientX, y: event.clientY });
+    const rect = event.target.getBoundingClientRect();
+    setPosition({ x: rect.left, y: rect.top - rect.height - 10 });
   };
-
-
-  function voltarParaPrimeiraLista() {
-    setExibirX(true);
-    setPrimeiroHorarioSelecionado(false);
-  }
+  
 
   const handleHorarioSelecionado = (horario) => {
-    if (!primeiroHorarioSelecionado) {
-      setPrimeiroHorarioSelecionado(true);
+    if (!horarioInicial) {
       setHorarioInicial(horario);
-      setHorarioSelecionado(horario); // apenas o primeiro horário é selecionado
-    } else {
+    } else if (!horarioFinal) {
       setHorarioFinal(horario);
       setShowHorarios(false);
-      setHorarioSelecionado(`${horarioInicial} até ${horario}`); // agora usa uma string composta
-      setHorarioInicial("");
-      setHorarioFinal("");
-      setPrimeiroHorarioSelecionado(false);
+      setHorarioSelecionado(`${horarioInicial} até ${horario}`);
     }
   };
-
 
   const handleFecharClick = () => {
     setShowHorarios(false);
     setHorarioInicial("");
     setHorarioFinal("");
-    setPrimeiroHorarioSelecionado(false);
-    setExibirX(true);
   };
 
   const handleReservarClick = () => {
-    if (horarioInicial && horarioFinal) { // agora verifica ambos os horários
-      setReservas([...reservas, `${horarioInicial} até ${horarioFinal}`]); // usa string composta
-      setHorarioReservado(`${horarioInicial} até ${horarioFinal}`); // usa string composta
+    if (horarioInicial && horarioFinal) {
+      alert("Reserva realizada com sucesso!");
+      setHorarioSelecionado("");
       setHorarioInicial("");
       setHorarioFinal("");
-      setPrimeiroHorarioSelecionado(false);
-      alert("Reserva realizada com sucesso!");
     } else {
       alert("Selecione os dois horários antes de reservar.");
     }
   };
 
-
-  const horarioLimite = 21.5 * 60; // 21:30 em minutos
-
   const horarios = [];
   for (let hora = 7.5; hora <= 22; hora += 0.5) {
     const horaInteira = Math.floor(hora);
     const minutos = hora === horaInteira ? "00" : "30";
-    const horario = `${horaInteira.toString().padStart(2, "0")}:${minutos}`;
-    const horarioEmMinutos = horaInteira * 60 + (minutos === "00" ? 0 : 30);
-    if (horarioEmMinutos > horarioLimite) break;
-    horarios.push(horario);
+    horarios.push(`${horaInteira.toString().padStart(2, "0")}:${minutos}`);
   }
-
 
   return (
     <div className="button-container">
       <div className="button-wrapper">
         <button className="horario" onClick={handleHorariosClick}>
-          {horarioReservado ? "Reservado" : (horarioSelecionado ? (primeiroHorarioSelecionado ? `${horarioInicial} até ${horarioFinal}` : horarioSelecionado) : "Horários")}
+          {horarioSelecionado || "Horários"}
         </button>
       </div>
       {showHorarios && (
-        <div className={`horarios-container ${primeiroHorarioSelecionado ? 'segunda-lista' : ''}`} style={{ left: position.x, top: position.y }}>
+        <div className="horarios-container" style={{ left: position.x, top: position.y }}>
           <span className="fechar" onClick={handleFecharClick}>X</span>
           {horarios
             .filter((horario) => {
-              if (!horarioSelecionado || !primeiroHorarioSelecionado) {
+              if (!horarioInicial) {
                 return true;
               } else {
                 const [hora, minutos] = horario.split(":").map(Number);
-                const [primeiroHora, primeiroMinutos] = horarioSelecionado.split(":").map(Number);
+                const [primeiroHora, primeiroMinutos] = horarioInicial.split(":").map(Number);
                 const primeiroHorarioEmMinutos = primeiroHora * 60 + primeiroMinutos;
                 const horarioEmMinutos = hora * 60 + minutos;
                 return horarioEmMinutos > primeiroHorarioEmMinutos + 30;
@@ -101,18 +76,13 @@ export default function Button(props) {
                 {horario}
               </button>
             ))}
-          {primeiroHorarioSelecionado && (
-            <div className="voltar-wrapper">
-              <button className="voltar" onClick={() => voltarParaPrimeiraLista(false)}>Voltar</button>
-            </div>
-          )}
         </div>
       )}
       <div className="button-wrapper">
-        <button className={`reserva${horarioSelecionado && horarioFinal ? " habilitado" : ""}`} onClick={handleReservarClick} disabled={!horarioSelecionado}>
-          Reservar
-        </button>
-      </div>
-    </div>
-  );
+        <button className={`reserva${horarioSelecionado && horarioFinal ? " habilitado" : ""}`} onClick={handleReservarClick} disabled={!horarioInicial || !horarioFinal}>
+Reservar
+</button>
+</div>
+</div>
+);
 }
