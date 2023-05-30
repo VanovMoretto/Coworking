0878ac43851de27aec0147553ccf0257f18e2840
Vanov Moretto - Dutra Corporativo <vanov@dutracorporativo.com.br>
2023-05-30 17:05:47 -0300

    updates

--- src/components/UserComps/MyBookingDisplay.jsx
+++ src/components/UserComps/MyBookingDisplay.jsx
@@ -5,9 +5,19 @@ const MyBookingDisplay = ({ booking }) => {
     <div className='booking-display'>
       <p className='booking-roomName'>Sala de reuniões: {booking.room}</p>
       <p className='booking-day'>Dia: {dayjs(booking.initialTime.toDate()).format('DD/MM/YYYY')}</p>
-      <p className='booking-time'>{`Reserva das ${dayjs(booking.initialTime.toDate()).format('HH:mm')} até ${dayjs(booking.finalTime.toDate()).format('HH:mm')}`}</p>
+      <p className='booking-time'>{`Horário: ${dayjs(booking.initialTime.toDate()).format('HH:mm')} até ${dayjs(booking.finalTime.toDate()).format('HH:mm')}`}</p>
     </div>
   );
 };
 
-export default MyBookingDisplay;
+const PastBookingDisplay = ({ booking }) => {
+  return (
+    <div className='past-booking-display'>
+      <p className='past-roomName'><strong>Local:</strong> {booking.room} -</p>
+      <p className='past-day'>&nbsp;<strong>Dia:</strong> {dayjs(booking.initialTime.toDate()).format('DD/MM/YYYY')}</p>
+      <p className='past-time'>&nbsp;- {`${dayjs(booking.initialTime.toDate()).format('HH:mm')}`}<strong> até </strong>{`${dayjs(booking.finalTime.toDate()).format('HH:mm')}`}</p>
+    </div>
+  );
+};
+
+export { MyBookingDisplay, PastBookingDisplay };
--- src/components/UserComps/MyBookingsPage.jsx
+++ src/components/UserComps/MyBookingsPage.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import MyBookingDisplay from "./MyBookingDisplay";
+import React, { useEffect, useState } from "react";
+import { MyBookingDisplay, PastBookingDisplay } from "./MyBookingDisplay";
 import RequireLogin from "../../utils/RequireLogin";
 import { useNavigate } from "react-router-dom";
 import { collection, query, where, getDocs, doc, deleteDoc } from 'firebase/firestore';
@@ -9,65 +9,11 @@ import dayjs from 'dayjs';
 import Sala from '../../utils/Sala';
 import '../../Styles/BookingPage.css'
 
-const MyBookingPage = () => {
-  const [bookings, setBookings] = React.useState([]);
-  const navigate = useNavigate();
-  const auth = getAuth();
 
-  useEffect(() => {
-
-    if (!auth.currentUser) {
-      // Redireciona o usuário para a página de login se ele não estiver logado
-      navigate("/requireLogin");
-    } else {
-      const fetchBookings = async () => {
-        const user = auth.currentUser;
-    
-        if (user) {
-          const q = query(collection(db, 'reservations'), where('userId', '==', user.uid));
-          const querySnapshot = await getDocs(q);
-          const fetchedBookings = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-    
-          // Ordena as reservas por horário de início
-          const sortedBookings = fetchedBookings.sort((a, b) => {
-            return dayjs(a.initialTime.toDate()).isAfter(dayjs(b.initialTime.toDate())) ? 1 : -1;
-          });
-    
-          setBookings(sortedBookings);
-        }
-      };
-    
-      fetchBookings();
-    }
-    
-  }, [auth, navigate]);
-
-  const delBooking = async (id) => {
-    if (window.confirm("Tem certeza que deseja cancelar sua reserva?")) {
-      const bookingRef = doc(db, 'reservations', id);
-  
-      try {
-        await deleteDoc(bookingRef);
-  
-        // Remover a reserva cancelada do estado
-        setBookings(prevBookings => prevBookings.filter(booking => booking.id !== id));
-      } catch (error) {
-        console.error("Erro ao cancelar reserva: ", error);
-      }
-    }
-  };
-  
-  
-// Verify if user is logged in before rendering the component
-if (!auth.currentUser) {
-  return <RequireLogin />;
-}
-
+const CurrentBookings = ({ bookings, delBooking }) => {
   return (
-    <div className="booking-main-container">
-      <div className="booking-title-container">
-        <h2 className="booking-title">Minhas Reservas</h2>
-      </div>
+    <div className="current-bookings-container">
+      <h3 className="booking-h3">Próximas reservas</h3>
       {bookings.map((booking, index) => (
         <div key={index} className="booking-box">
           <div className="booking-infos">
@@ -79,10 +25,103 @@ if (!auth.currentUser) {
             </div>
           </div>
           <div className="booking-button">
-            <button className="cancel-button" onClick={() => delBooking(booking.id)}>Cancelar</button>
+            <button className="cancel-button" onClick={() => delBooking(booking.id, false)}>Cancelar</button>
           </div>
         </div>
       ))}
+    </div>
+  )
+}
+
+const PastBookings = ({ bookings, delBooking }) => {
+  return (
+    <div className="past-bookings-container">
+      <h3 className="booking-h3">Histórico de reservas</h3>
+      {bookings.map((booking, index) => (
+        <div key={index} className="past-booking-box">
+          <div className="past-booking-infos">
+            <div>
+              <PastBookingDisplay booking={booking} />
+            </div>
+            <button className="erase-button" onClick={() => delBooking(booking.id, true)}>Apagar</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const MyBookingPage = () => {
+  const [bookings, setBookings] = useState([]);
+  const [pastBookings, setPastBookings] = useState([]);
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+
+    if (!auth.currentUser) {
+      // Redireciona o usuário para a página de login se ele não estiver logado
+      navigate("/requireLogin");
+    } else {
+      const fetchBookings = async () => {
+        const user = auth.currentUser;
+
+        if (user) {
+          const q = query(collection(db, 'reservations'), where('userId', '==', user.uid));
+          const querySnapshot = await getDocs(q);
+          const fetchedBookings = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+
+          const currentBookings = [];
+          const oldBookings = [];
+
+          // Divide as reservas em futuras e passadas
+          fetchedBookings.forEach(booking => {
+            if (dayjs(booking.initialTime.toDate()).isAfter(dayjs())) {
+              currentBookings.push(booking);
+            } else {
+              oldBookings.push(booking);
+            }
+          });
+
+          setBookings(currentBookings);
+          setPastBookings(oldBookings);
+        }
+      };
+
+      fetchBookings();
+    }
+
+  }, [auth, navigate]);
+
+  const delBooking = async (id, isPast) => { // Adicionamos o argumento "isPast"
+    if (window.confirm(isPast ? "Deseja apagar a reserva do histórico?" : "Tem certeza que deseja cancelar sua reserva?")) {
+      const bookingRef = doc(db, 'reservations', id);
+      try {
+        await deleteDoc(bookingRef);
+        if (isPast) {
+          setPastBookings(prevBookings => prevBookings.filter(booking => booking.id !== id));
+        } else {
+          setBookings(prevBookings => prevBookings.filter(booking => booking.id !== id));
+        }
+      } catch (error) {
+        console.error("Erro ao cancelar reserva: ", error);
+      }
+    }
+  };
+
+
+  // Verify if user is logged in before rendering the component
+  if (!auth.currentUser) {
+    return <RequireLogin />;
+  }
+
+  return (
+    <div className="booking-main-container">
+      <div className="booking-title-container">
+        <h2 className="booking-title">Minhas Reservas</h2>
+      </div>
+      <CurrentBookings bookings={bookings} delBooking={delBooking} />
+      <PastBookings bookings={pastBookings} delBooking={delBooking} />
     </div>
   )
 }
